2a914cc133ff4d4ed7ee7dd3c75f29b5d52fb1f4
Chandpasha <160645454+Chandpashashaik115@users.noreply.github.com>
2024-10-19 11:33:58 +0530

    Update streamlitvid.py

--- streamlitvid.py
+++ streamlitvid.py
@@ -1,15 +1,11 @@
-import cv2
-import numpy as np
 import streamlit as st
 from streamlit_webrtc import VideoTransformerBase, webrtc_streamer
 
 # Define a VideoTransformer class to process the video frames
 class VideoTransformer(VideoTransformerBase):
     def transform(self, frame):
-        # Convert the frame from BGR (OpenCV) to RGB
         img = frame.to_ndarray(format="bgr")
-        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-        return img
+        return img  # Directly return the frame for now
 
 def main():
     st.title("Webcam Stream using WebRTC")
